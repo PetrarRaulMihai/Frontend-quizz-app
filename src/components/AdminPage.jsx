9f2f561e08dbc266cb593d6a790a747571b83bb7
Caldarescu Iulian <caldarescuiulian@gmail.com>
2023-12-19 00:46:08 +0200

    admin page with add subject and questions done

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,30 +1,15 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import Subject from "./Subject";
 import supabase from "../config/supabaseConfig";
 import { FaCirclePlus } from "react-icons/fa6";
 import { FaSpinner } from "react-icons/fa";
 import { useFormik } from "formik";
-import { object } from "yup";
 
 function Subjects() {
   const [quizArray, setQuizArray] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [subjectSelected, setSubjectSelected] = useState("");
-  const inputRef = useRef();
 
-  const [optionObject, setOptionObject] = useState({
-    answer: "",
-    options: [],
-    question: "",
-  });
-
-  const formik = useFormik({
-    initialValues: {
-      title: "",
-      icon: "",
-      questions: [],
-    },
-  });
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     const fetchSupa = async () => {
@@ -39,17 +24,61 @@ function Subjects() {
     };
 
     fetchSupa();
+    console.log(quizArray);
   }, []);
 
-  // add options in array of options
-  const addOption = () => {
-    setOptionObject({
-      ...optionObject,
-      options: [...optionObject.options, inputRef.current.value],
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      icon: "",
+      question: "",
+      answer: "",
+      option1: "",
+      option2: "",
+      option3: "",
+      option4: "",
+      questions: [],
+    },
+  });
+
+  const addQuestion = () => {
+    const newQuestion = {
+      question: formik.values.question,
+      answer: formik.values.answer,
+      options: [
+        formik.values.option1,
+        formik.values.option2,
+        formik.values.option3,
+        formik.values.option4,
+      ],
+    };
+
+    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+
+    formik.setFieldValue("question", "");
+    formik.setFieldValue("answer", "");
+    formik.setFieldValue("option1", "");
+    formik.setFieldValue("option2", "");
+    formik.setFieldValue("option3", "");
+    formik.setFieldValue("option4", "");
+  };
+
+  useEffect(() => {
+    formik.setValues({
+      ...formik.values,
+      questions: questions,
     });
-    console.log(optionObject);
-    inputRef.current.value = "";
-    inputRef.current.focus();
+  }, [questions]);
+
+  const submitAllData = async () => {
+    const { error } = await supabase.from("QuizzApp-Quizz").insert({
+      title: formik.values.title,
+      icon: formik.values.icon,
+      questions: formik.values.questions,
+    });
+
+    console.log(formik.values.questions);
+    formik.resetForm();
   };
 
   return (
@@ -63,45 +92,97 @@ function Subjects() {
         </div>
         <p className="text-lg">Add new subject</p>
       </div>
-      {/* <select onChange={handleChange}>
-        {quizArray.map((item) => {
-          return <option value={item.title}>{item.title}</option>;
-        })}
-      </select> */}
-
-      {/* DE PROBA de aici se continua ------------------------------------------- */}
-      <div>{/* <p>{subjectSelected}</p> */}</div>
 
       {/* Add subject form */}
-      {true && (
-        <form onSubmit={formik.handleSubmit} className="flex flex-col gap-5">
-          {/* Title */}
-          <input type="text" placeholder="Subject title"></input>
-          {/* Icon */}
-          <input type="text" placeholder="Icon"></input>
 
-          {/* Questions $ answers */}
-          <div className="flex flex-col gap-5">
-            <input type="text" placeholder="Question"></input>
-            <input type="text" placeholder="Answer"></input>
-            <div>
-              <input
-                ref={inputRef}
-                name="options"
-                type="text"
-                placeholder="Option"
-              ></input>{" "}
-              <button
-                onClick={addOption}
-                type="submit"
-                className="bg-green-500"
-              >
-                ADD
-              </button>
-            </div>
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-5">
+        {/* Title */}
+        <input
+          type="text"
+          placeholder="Subject title"
+          name="title"
+          onChange={formik.handleChange}
+          value={formik.values.title}
+        ></input>
+        {/* Icon */}
+        <input
+          type="text"
+          placeholder="Icon"
+          name="icon"
+          onChange={formik.handleChange}
+          value={formik.values.icon}
+        ></input>
+
+        <div className="flex flex-col gap-5">
+          <input
+            type="text"
+            placeholder="Question"
+            name="question"
+            onChange={formik.handleChange}
+            value={formik.values.question}
+          ></input>
+          <input
+            type="text"
+            placeholder="Answer"
+            name="answer"
+            onChange={formik.handleChange}
+            value={formik.values.answer}
+          ></input>
+          {/* option inputs */}
+          <div>
+            <input
+              name="option1"
+              type="text"
+              placeholder="Option1"
+              onChange={formik.handleChange}
+              value={formik.values.option1}
+            ></input>{" "}
+            <input
+              name="option2"
+              type="text"
+              placeholder="Option2"
+              onChange={formik.handleChange}
+              value={formik.values.option2}
+            ></input>{" "}
+            <input
+              name="option3"
+              type="text"
+              placeholder="Option3"
+              onChange={formik.handleChange}
+              value={formik.values.option3}
+            ></input>{" "}
+            <input
+              name="option4"
+              type="text"
+              placeholder="Option4"
+              onChange={formik.handleChange}
+              value={formik.values.option4}
+            ></input>{" "}
           </div>
-        </form>
-      )}
+        </div>
+
+        <button type="button" onClick={addQuestion}>
+          Add question
+        </button>
+
+        <div>
+          <button
+            className="bg-green-500 p-4"
+            type="button"
+            onClick={submitAllData}
+          >
+            Submit all data
+          </button>
+        </div>
+      </form>
+
+      {/* end of form. building the delete function */}
+
+      <div>
+        {quizArray.map((quizz) => {
+          return <p>{quizz.title}</p>;
+        })}
+      </div>
     </div>
   );
 }
